ea33f23b4461122052f3821122799628f7d341b0
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2023-05-21 18:22:07 -0300

    feat: recomendation plp

---
+++ sections/ProductSimpleShelf.tsx
@@ -0,0 +1,9 @@
+import ProductSimpleShelf, {
+  Props,
+} from "deco-sites/fashion/components/product/ProductSimpleShelf.tsx";
+
+function ProductShelfSection(props: Props) {
+  return <ProductSimpleShelf {...props} />;
+}
+
+export default ProductShelfSection;
