0488ccb484b9a6d79461cf5c321f2c2159aa89b8
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-05-21 16:14:21 -0300

    feat: banner movie

---
+++ sections/BannerMovie.tsx
@@ -0,0 +1,6 @@
+export { default } from "deco-sites/fashion/components/ui/BannerMovie.tsx";
+export type {
+  Movie,
+  Props,
+} from "deco-sites/fashion/components/ui/BannerMovie.tsx";
+export type { Image as LiveImage } from "deco-sites/std/components/types.ts";
