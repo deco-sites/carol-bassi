59f577f0f1d3103b5090219cee814859c5b97880
Diego Oliveira <Diego.o.guimaraes29@gmail.com>
2023-05-22 09:49:16 -0300

    FiX: header navbar

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -137,7 +137,6 @@ function colorHeader() {
     }
   });
 }
-
 function Header(
   {
     alerts,
--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -1,11 +1,19 @@
 import Image from "deco-sites/std/components/Image.tsx";
 import { headerHeight } from "./constants.ts";
-
 export interface INavItem {
   label: string;
   href: string;
   children?: INavItem[];
   image?: { src?: string; alt?: string };
+}
+
+function pathAtual() {
+  let navB = "";
+
+  globalThis.addEventListener("load", () => {
+    navB = window.location.pathname;
+  });
+  return navB;
 }
 
 function NavItem({ item }: { item: INavItem }) {
@@ -22,38 +30,43 @@ function NavItem({ item }: { item: INavItem }) {
       {children && children.length > 0 &&
         (
           <div
-            class="fixed hidden hover:flex group-hover:flex bg-base-100 z-50 items-start justify-center gap-6 border-t border-b-2 border-base-200 w-screen"
-            style={{ top: "0px", left: "0px", marginTop: headerHeight }}
+            class={`fixed hidden mt-[110px] hover:flex group-hover:flex bg-base-100 z-50 items-center justify-center gap-6 border-t border-b-2 border-base-200 w-screen`}
+            nav-height=""
+            style={{ top: "0px", left: "0px" }}
           >
-            {image?.src && (
-              <Image
-                class="p-6"
-                src={image.src}
-                alt={image.alt}
-                width={300}
-                height={332}
-                loading="lazy"
-              />
-            )}
-            <ul class="flex items-start justify-center gap-6">
-              {children.map((node) => (
-                <li class="p-6">
-                  <a class="hover:underline" href={node.href}>
-                    <span>{node.label}</span>
-                  </a>
+            <div class="w-full min-h-[350px]">
+              <ul class="flex items-start justify-center gap-6">
+                {children.map((node) => (
+                  <li class="p-6">
+                    <a class="hover:underline" href={node.href}>
+                      <span>{node.label}</span>
+                    </a>
 
-                  <ul class="flex flex-col gap-1 mt-4">
-                    {node.children?.map((leaf) => (
-                      <li>
-                        <a class="hover:underline" href={leaf.href}>
-                          <span class="text-xs">{leaf.label}</span>
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
+                    <ul class="flex flex-col gap-5 flex-wrap max-h-[250px] mt-4">
+                      {node.children?.map((leaf) => (
+                        <li>
+                          <a class="hover:underline" href={leaf.href}>
+                            <span class="text-xs">{leaf.label}</span>
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div class="w-full h-[250px] flex flex-row justify-start items-center border-secondary-black  border-l-2 ">
+              {image?.src && (
+                <Image
+                  class="p-6"
+                  src={image.src}
+                  alt={image.alt}
+                  width={300}
+                  height={332}
+                  loading="lazy"
+                />
+              )}
+            </div>
           </div>
         )}
     </li>
